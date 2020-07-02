9b416a40fd27fa1cf12ed8b417eb15b55a807e79
kaspgreta <greta.kasparaityte@gmail.com>
2020-07-02 10:57:13 +0300

    Add files via upload

---
+++ technicalTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace technicalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(n);
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
